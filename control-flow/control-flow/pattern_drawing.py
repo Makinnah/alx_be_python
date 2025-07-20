f6afc438147c2c8adfc6322b217a136e50d06e65
Duncan Makinnah Osuka <makinnahdavid@gmail.com>
2025-07-20 23:23:59 +0300

    Updated pattern_drawing.py

--- control-flow/control-flow/pattern_drawing.py
+++ control-flow/control-flow/pattern_drawing.py
@@ -1,1 +1,11 @@
-pattern drawing code will be here
+# Ask the user for the size of the pattern
+size = int(input("Enter the size of the pattern: "))
+
+# Use a while loop to handle rows
+row = 0
+while row < size:
+    # Use a for loop to print asterisks in each row
+    for col in range(size):
+        print("*", end="")
+    print()  # Move to the next line after each row
+    row += 1
