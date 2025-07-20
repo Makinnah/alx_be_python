eb6a20b903075ad053810552fa669ad00bae2efd
Duncan Makinnah Osuka <makinnahdavid@gmail.com>
2025-07-20 22:42:12 +0300

    Add weather advice logic using conditionals

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,1 +1,12 @@
-#weather advice script will go here
+# Ask the user about the weather
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+
+# Use conditional statements to give advice
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
